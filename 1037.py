c2aa9b96d147a6ea013a979052e5e7d2a5339c0e
김정민 <jmk445123456@gmail.com>
2023-11-18 13:46:55 +0900

    feat: 1268.py 추가

---
+++ 1037.py
@@ -0,0 +1,8 @@
+N = int(input())
+A = list(map(int, input().split()))
+
+print(A)
+max_num = max(A)
+min_num = min(A)
+
+print(max_num * min_num)
---
+++ 1268.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int N;
+    cin >> N;
+    
+    int[5] 
+    int* Arr = new int[N];
+
+
+
+
+    for (int i = 0; i < N; i++){
+
+    }
+}
